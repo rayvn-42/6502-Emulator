37d48b514acb0c75fd6abae155c1db4849e0e78f
rayvn-42 <rayanexpro7@gmail.com>
2025-04-07 22:47:50 +0100

    Added Instructions

--- Computer/Cpu.py
+++ Computer/Cpu.py
@@ -18,13 +18,41 @@ class CPU:
         self.INS_LDA_ZP: Byte = 0xA5
         '''(Load A zero page) Loads a value into the A register from the zero page,    Cycles: 3'''
         self.INS_LDA_ZPX: Byte = 0xB5
-        '''(Load A zero page X) Loads a value into the A register from the ZP address + value in X register,    Cycles: 4'''
+        '''(Load A zero page, X) Loads a value into the A register from the ZP address + value in X register,    Cycles: 4'''
         self.INS_LDA_ABS: Byte = 0xAD
         '''(Load A absolute) Loads a value into the A register using a 16-bit address,    Cycles: 4'''
         self.INS_LDA_ABSX: Byte = 0xBD
         '''(Load A absolute, X) Loads a value into the A register using a 16-bit address + value in X register,    Cycles: 4 (+1 if page crossed)'''
+        self.INS_LDA_ABSY: Byte = 0xB9
+        '''(Load A absolute, Y) Loads a value into the A register using a 16-bit address + value in Y register,    Cycles: 4 (+1 if page crossed)'''
+        self.INS_LDA_INDX: Byte = 0xA1
+        '''(Load A indirect X) Loads a value into the A register using a ZP pointer = ZP address + value in X register, Cycles: 6'''
+        self.INS_LDA_INDY: Byte = 0xB1
+        '''(Load A indirect Y) Loads a value into the A register using a ZP pointer = ZP address + value in Y register, Cycles: 5 (+1 if page crossed)'''
+        self.INS_LDX_IM: Byte = 0xA2
+        '''(Load X immediate) Loads a value into the X register,    Cycles: 2'''
+        self.INS_LDX_ZP: Byte = 0xA6
+        '''(Load X zero page) Loads a value into the X register from the zero page,    Cycles: 3'''
+        self.INS_LDX_ZPY: Byte = 0xB6
+        '''(Load X zero page, Y) Loads a value into the X register from the ZP address + value in Y register,    Cycles: 4'''
+        self.INS_LDX_ABS: Byte = 0xAE
+        '''(Load X absolute) Loads a value into the X register using a 16-bit address,    Cycles: 4'''
+        self.INS_LDX_ABSY: Byte = 0xBE
+        '''(Load X absolute, Y) Loads a value into the X register using a 16-bit address + value in Y register,    Cycles: 4 (+1 if page crossed)'''
+        self.INS_LDY_IM: Byte = 0xA0
+        '''(Load Y immediate) Loads a value into the Y register,    Cycles: 2'''
+        self.INS_LDY_ZP: Byte = 0xA4
+        '''(Load Y zero page) Loads a value into the Y register from the zero page,    Cycles: 3'''
+        self.INS_LDY_ZPX: Byte = 0xB4
+        '''(Load Y zero page, X) Loads a value into the Y register from the ZP address + value in X register,    Cycles: 4'''
+        self.INS_LDY_ABS: Byte = 0xAC
+        '''(Load Y absolute) Loads a value into the Y register using a 16-bit address,    Cycles: 4'''
+        self.INS_LDY_ABSX: Byte = 0xBC
+        '''(Load Y absolute, X) Loads a value into the Y register using a 16-bit address + value in X register,    Cycles: 4 (+1 if page crossed)'''
         self.INS_JSR: Byte = 0x20
         '''(Jump to subroutine) Pushes an address to the stack then jumps to that address in memory    Cycles: 4'''
+        self.INS_NOP: Byte = 0xEA
+        '''(No operation) Increments the program counter and does nothing,    Cycles: 2'''
         self.PLATFORM_BIG_ENDIAN = (False if byteorder == "little" else True)
 
     def get_flag(self, bit: int) -> int:
@@ -121,6 +149,14 @@ class CPU:
     def LDASetStatus(self):
         self.Z_flag = (True if self.A_reg == 0 else False)
         self.N_flag = (True if self.A_reg & 0b10000000 else False)
+    
+    def LDXSetStatus(self):
+        self.Z_flag = (True if self.X_reg == 0 else False)
+        self.N_flag = (True if self.X_reg & 0b10000000 else False)
+
+    def LDYSetStatus(self):
+        self.Z_flag = (True if self.Y_reg == 0 else False)
+        self.N_flag = (True if self.Y_reg & 0b10000000 else False)
 
     def exec(self, memory: Mem, cycles: s32) -> s32:
         start_cycles: s32 = cycles
@@ -158,11 +194,104 @@ class CPU:
                     Value: Byte = self.readByte( memory, Address, cycles_lst )
                     self.A_reg = Value
                     self.LDASetStatus()
+                case self.INS_LDA_ABSY:
+                    LSB_Byte: Byte = self.fetchByte( memory, cycles_lst )
+                    MSB_Byte: Byte = self.fetchByte( memory, cycles_lst )
+                    BaseAddress: Word = LSB_Byte | (MSB_Byte << 8)
+                    Address: Word = BaseAddress + self.Y_reg
+                    if (BaseAddress & 0xFF00) != (Address & 0xFF00):
+                        cycles_lst[0] -= 1
+                    Value: Byte = self.readByte( memory, Address, cycles_lst )
+                    self.A_reg = Value
+                    self.LDASetStatus()
+                case self.INS_LDA_INDX:
+                    ZeroPageAddress: Byte = self.fetchByte( memory, cycles_lst )
+                    ZP_Pointer: Byte = (ZeroPageAddress + self.X_reg) & 0xFF
+                    cycles_lst[0] -= 1
+                    LSB_Byte: Byte = self.readByte( memory, ZP_Pointer, cycles_lst )
+                    MSB_Byte: Byte = self.readByte( memory, (ZP_Pointer + 1) & 0xFF, cycles_lst )
+                    Address: Word = (MSB_Byte << 8) | LSB_Byte
+                    Value: Byte = self.readByte( memory, Address, cycles_lst )
+                    self.A_reg = Value
+                    self.LDASetStatus()
+                case self.INS_LDA_INDY:
+                    ZeroPageAddress: Byte = self.fetchByte( memory, cycles_lst )
+                    ZP_Pointer: Byte = ZeroPageAddress
+                    LSB_Byte: Byte = self.readByte( memory, ZP_Pointer, cycles_lst )
+                    MSB_Byte: Byte = self.readByte( memory, (ZP_Pointer + 1) & 0xFF, cycles_lst )
+                    BaseAddress: Word = (MSB_Byte << 8) | LSB_Byte
+                    Address: Word = BaseAddress + self.Y_reg
+                    if (BaseAddress & 0xFF00) != (Address & 0xFF00):
+                        cycles_lst[0] -= 1
+                    Value: Byte = self.readByte( memory, Address, cycles_lst )
+                    self.A_reg = Value
+                    self.LDASetStatus()
+                case self.INS_LDX_IM:
+                    Value: Byte = self.fetchByte( memory, cycles_lst )
+                    self.X_reg = Value
+                    self.LDXSetStatus()
+                case self.INS_LDX_ZP:
+                    ZeroPageAddress: Byte = self.fetchByte( memory, cycles_lst )
+                    self.X_reg = self.readByte( memory, ZeroPageAddress, cycles_lst )
+                    self.LDXSetStatus()
+                case self.INS_LDX_ZPY:
+                    ZeroPageAddress: Byte = self.fetchByte( memory, cycles_lst )
+                    ZeroPageAddress = (ZeroPageAddress + self.Y_reg) & 0xFF
+                    cycles_lst[0] -= 1
+                    self.X_reg = self.readByte( memory, ZeroPageAddress, cycles_lst )
+                    self.LDXSetStatus()
+                case self.INS_LDX_ABS:
+                    LSB_Byte: Byte = self.fetchByte( memory, cycles_lst )
+                    MSB_Byte: Byte = self.fetchByte( memory, cycles_lst )
+                    Address: Word = LSB_Byte | (MSB_Byte << 8)
+                    self.X_reg = self.readByte( memory, Address, cycles_lst )
+                    self.LDXSetStatus()
+                case self.INS_LDX_ABSY:
+                    LSB_Byte: Byte = self.fetchByte( memory, cycles_lst )
+                    MSB_Byte: Byte = self.fetchByte( memory, cycles_lst )
+                    BaseAddress: Word = LSB_Byte | (MSB_Byte << 8)
+                    Address: Word = BaseAddress + self.Y_reg
+                    if (BaseAddress & 0xFF00) != (Address & 0xFF00):
+                        cycles_lst[0] -= 1
+                    self.X_reg = self.readByte( memory, Address, cycles_lst )
+                    self.LDXSetStatus()
+                case self.INS_LDY_IM:
+                    Value: Byte = self.fetchByte( memory, cycles_lst )
+                    self.Y_reg = Value
+                    self.LDYSetStatus()
+                case self.INS_LDY_ZP:
+                    ZeroPageAddress: Byte = self.fetchByte( memory, cycles_lst )
+                    self.Y_reg = self.readByte( memory, ZeroPageAddress, cycles_lst )
+                    self.LDYSetStatus()
+                case self.INS_LDY_ZPX:
+                    ZeroPageAddress: Byte = self.fetchByte( memory, cycles_lst )
+                    ZeroPageAddress = (ZeroPageAddress + self.X_reg) & 0xFF
+                    cycles_lst[0] -= 1
+                    self.Y_reg = self.readByte( memory, ZeroPageAddress, cycles_lst )
+                    self.LDYSetStatus()
+                case self.INS_LDY_ABS:
+                    LSB_Byte: Byte = self.fetchByte( memory, cycles_lst )
+                    MSB_Byte: Byte = self.fetchByte( memory, cycles_lst )
+                    Address: Word = LSB_Byte | (MSB_Byte << 8)
+                    self.Y_reg = self.readByte( memory, Address, cycles_lst )
+                    self.LDYSetStatus()
+                case self.INS_LDY_ABSX:
+                    LSB_Byte: Byte = self.fetchByte( memory, cycles_lst )
+                    MSB_Byte: Byte = self.fetchByte( memory, cycles_lst )
+                    BaseAddress: Word = LSB_Byte | (MSB_Byte << 8)
+                    Address: Word = BaseAddress + self.X_reg
+                    if (BaseAddress & 0xFF00) != (Address & 0xFF00):
+                        cycles_lst[0] -= 1
+                    self.Y_reg = self.readByte( memory, Address, cycles_lst )
+                    self.LDYSetStatus()
                 case self.INS_JSR:
                     SubAddr: Word = self.fetchWord( memory, cycles_lst )
                     self.SP -= 2
                     memory.WriteWord( cycles_lst, self.SP, self.PC - 1 )
                     self.PC = SubAddr
+                    cycles_lst[0] -= 1
+                case self.INS_NOP:
+                    self.PC += 1
                     cycles_lst[0] -= 1
                 case _:
                     print(f"Instruction not handled: {Ins}")
--- main.py
+++ main.py
@@ -20,6 +20,20 @@ class TestComputer(unittest.TestCase):
         self.assertFalse(self.cpu.V_flag)
         self.assertFalse(self.cpu.C_flag)
 
+    def VerifyFlags_NoMod_LDX(self):
+        self.assertFalse(self.cpu.I_flag)
+        self.assertFalse(self.cpu.D_flag)
+        self.assertFalse(self.cpu.B_flag)
+        self.assertFalse(self.cpu.V_flag)
+        self.assertFalse(self.cpu.C_flag)
+
+    def VerifyFlags_NoMod_LDY(self):
+        self.assertFalse(self.cpu.I_flag)
+        self.assertFalse(self.cpu.D_flag)
+        self.assertFalse(self.cpu.B_flag)
+        self.assertFalse(self.cpu.V_flag)
+        self.assertFalse(self.cpu.C_flag)
+
     def test_LDA_IM(self):
         self.mem[0xFFFC] = self.cpu.INS_LDA_IM
         self.mem[0xFFFD] = 0x84
@@ -96,9 +110,7 @@ class TestComputer(unittest.TestCase):
         self.mem[0x0145 + self.cpu.X_reg] = 0x84
         CyclesUsed = self.cpu.exec(self.mem, 4)
         self.assertEqual(self.cpu.A_reg, 0x84)
-        self.assertIn(CyclesUsed, [4, 5])  # Page crossing possible
-        self.assertFalse(self.cpu.Z_flag)
-        self.assertTrue(self.cpu.N_flag)
+        self.assertIn(CyclesUsed, [4, 5])
         self.VerifyFlags_NoMod_LDA()
 
     def test_LDA_ABSX_Wrap(self):
@@ -106,13 +118,232 @@ class TestComputer(unittest.TestCase):
         self.mem[0xFFFC] = self.cpu.INS_LDA_ABSX
         self.mem[0xFFFD] = 0xF5
         self.mem[0xFFFE] = 0xFF
-        self.mem[(0xFFF5 + self.cpu.X_reg) & 0xFFFF] = 0x84
-        CyclesUsed = self.cpu.exec(self.mem, 4)
+        self.mem[0xFFF5 + self.cpu.X_reg] = 0x84
+        CyclesUsed = self.cpu.exec(self.mem, 5)
         self.assertEqual(self.cpu.A_reg, 0x84)
         self.assertIn(CyclesUsed, [4, 5])
-        self.assertFalse(self.cpu.Z_flag)
-        self.assertTrue(self.cpu.N_flag)
         self.VerifyFlags_NoMod_LDA()
+
+    def test_LDA_ABSY(self):
+        self.cpu.Y_reg = 0xA7
+        self.mem[0xFFFC] = self.cpu.INS_LDA_ABSY
+        self.mem[0xFFFD] = 0x87
+        self.mem[0xFFFE] = 0x01
+        self.mem[0x0187 + self.cpu.Y_reg] = 0x85
+        CyclesUsed = self.cpu.exec(self.mem, 4)
+        self.assertEqual(self.cpu.A_reg, 0x85)
+        self.assertIn(CyclesUsed, [4, 5])
+        self.VerifyFlags_NoMod_LDA()
+
+    def test_LDA_ABSY_Wrap(self):
+        self.cpu.Y_reg = 0x0F
+        self.mem[0xFFFC] = self.cpu.INS_LDA_ABSY
+        self.mem[0xFFFD] = 0xF5
+        self.mem[0xFFFE] = 0xFF
+        self.mem[0xFFF5 + self.cpu.Y_reg] = 0x85
+        CyclesUsed = self.cpu.exec(self.mem, 5)
+        self.assertEqual(self.cpu.A_reg, 0x85)
+        self.assertIn(CyclesUsed, [4, 5])
+        self.VerifyFlags_NoMod_LDA()
+
+    def test_LDA_INDX(self):
+        self.cpu.X_reg = 0x04
+        self.mem[0xFFFC] = self.cpu.INS_LDA_INDX
+        self.mem[0xFFFD] = 0x20
+        self.mem[0x0020 + self.cpu.X_reg] = 0x34
+        self.mem[(0x0020 + self.cpu.X_reg) + 0x01] = 0x12
+        self.mem[0x1234] = 0x99
+        CyclesUsed = self.cpu.exec(self.mem, 6)
+        self.assertEqual(self.cpu.A_reg, 0x99)
+        self.assertEqual(CyclesUsed, 6)
+        self.VerifyFlags_NoMod_LDA()
+
+    def test_LDA_INDX_Wrap(self):
+        self.cpu.X_reg = 0x0F
+        self.mem[0xFFFC] = self.cpu.INS_LDA_INDX
+        self.mem[0xFFFD] = 0xF1
+        self.mem[(0x00F1 + self.cpu.X_reg) & 0xFF] = 0x34
+        self.mem[((0x00F1 + self.cpu.X_reg) + 0x01) & 0xFF] = 0x12
+        self.mem[0x1234] = 0x99
+        CyclesUsed = self.cpu.exec(self.mem, 6)
+        self.assertEqual(self.cpu.A_reg, 0x99)
+        self.assertEqual(CyclesUsed, 6)
+        self.VerifyFlags_NoMod_LDA()
+
+    def test_LDA_INDY(self):
+        self.cpu.Y_reg = 0x10
+        self.mem[0xFFFC] = self.cpu.INS_LDA_INDY
+        self.mem[0xFFFD] = 0x20
+        self.mem[0x0020] = 0x00
+        self.mem[0x0021] = 0x80
+        self.mem[0x8010] = 0x99
+        CyclesUsed = self.cpu.exec(self.mem, 5)
+        self.assertEqual(self.cpu.A_reg, 0x99)
+        self.assertEqual(CyclesUsed, 5)
+        self.VerifyFlags_NoMod_LDA()
+
+    def test_LDA_INDY_Wrap(self):
+        self.cpu.Y_reg = 0xFF
+        self.mem[0xFFFC] = self.cpu.INS_LDA_INDY
+        self.mem[0xFFFD] = 0x20
+        self.mem[0x0020] = 0xF0
+        self.mem[0x0021] = 0x80
+        self.mem[0x81EF] = 0x55
+        CyclesUsed = self.cpu.exec(self.mem, 6)
+        self.assertEqual(self.cpu.A_reg, 0x55)
+        self.assertEqual(CyclesUsed, 6)
+        self.VerifyFlags_NoMod_LDA()
+
+    def test_LDX_IM(self):
+        self.mem[0xFFFC] = self.cpu.INS_LDX_IM
+        self.mem[0xFFFD] = 0x99
+        CyclesUsed = self.cpu.exec(self.mem, 2)
+        self.assertEqual(self.cpu.X_reg, 0x99)
+        self.assertEqual(CyclesUsed, 2)
+        self.VerifyFlags_NoMod_LDX()
+
+    def test_LDX_IM_ZERO(self):
+        self.cpu.X_reg = 0x44
+        self.mem[0xFFFC] = self.cpu.INS_LDX_IM
+        self.mem[0xFFFD] = 0x00
+        CyclesUsed = self.cpu.exec(self.mem, 2)
+        self.assertEqual(self.cpu.X_reg, 0x00)
+        self.assertEqual(CyclesUsed, 2)
+        self.VerifyFlags_NoMod_LDX()
+
+    def test_LDX_ZP(self):
+        self.mem[0xFFFC] = self.cpu.INS_LDX_ZP
+        self.mem[0xFFFD] = 0x42
+        self.mem[0x0042] = 0x37
+        CyclesUsed = self.cpu.exec(self.mem, 3)
+        self.assertEqual(self.cpu.X_reg, 0x37)
+        self.assertEqual(CyclesUsed, 3)
+        self.VerifyFlags_NoMod_LDX()
+
+    def test_LDX_ZPY(self):
+        self.cpu.Y_reg = 5
+        self.mem[0xFFFC] = self.cpu.INS_LDX_ZPY
+        self.mem[0xFFFD] = 0x42
+        self.mem[0x0047] = 0x37
+        CyclesUsed = self.cpu.exec(self.mem, 4)
+        self.assertEqual(self.cpu.X_reg, 0x37)
+        self.assertEqual(CyclesUsed, 4)
+        self.VerifyFlags_NoMod_LDX()
+
+    def test_LDX_ZPY_Wrap(self):
+        self.cpu.Y_reg = 0xFF
+        self.mem[0xFFFC] = self.cpu.INS_LDX_ZPY
+        self.mem[0xFFFD] = 0x80
+        self.mem[0x007F] = 0x37
+        CyclesUsed = self.cpu.exec(self.mem, 4)
+        self.assertEqual(self.cpu.X_reg, 0x37)
+        self.assertEqual(CyclesUsed, 4)
+        self.VerifyFlags_NoMod_LDX()
+
+    def test_LDX_ABS(self):
+        self.mem[0xFFFC] = self.cpu.INS_LDX_ABS
+        self.mem[0xFFFD] = 0x80
+        self.mem[0xFFFE] = 0x44
+        self.mem[0x4480] = 0x84
+        CyclesUsed = self.cpu.exec(self.mem, 4)
+        self.assertEqual(self.cpu.X_reg, 0x84)
+        self.assertEqual(CyclesUsed, 4)
+        self.VerifyFlags_NoMod_LDX()
+
+    def test_LDX_ABSY(self):
+        self.cpu.Y_reg = 0x45
+        self.mem[0xFFFC] = self.cpu.INS_LDX_ABSY
+        self.mem[0xFFFD] = 0x45
+        self.mem[0xFFFE] = 0x01
+        self.mem[0x0145 + self.cpu.Y_reg] = 0x84
+        CyclesUsed = self.cpu.exec(self.mem, 4)
+        self.assertEqual(self.cpu.X_reg, 0x84)
+        self.assertIn(CyclesUsed, [4, 5])
+        self.VerifyFlags_NoMod_LDX()
+
+    def test_LDX_ABSY_Wrap(self):
+        self.cpu.Y_reg = 0x0F
+        self.mem[0xFFFC] = self.cpu.INS_LDX_ABSY
+        self.mem[0xFFFD] = 0xF5
+        self.mem[0xFFFE] = 0xFF
+        self.mem[0xFFF5 + self.cpu.Y_reg] = 0x84
+        CyclesUsed = self.cpu.exec(self.mem, 5)
+        self.assertEqual(self.cpu.X_reg, 0x84)
+        self.assertIn(CyclesUsed, [4, 5])
+        self.VerifyFlags_NoMod_LDX()
+
+    def test_LDY_IM(self):
+        self.mem[0xFFFC] = self.cpu.INS_LDY_IM
+        self.mem[0xFFFD] = 0x95
+        CyclesUsed = self.cpu.exec(self.mem, 2)
+        self.assertEqual(self.cpu.Y_reg, 0x95)
+        self.assertEqual(CyclesUsed, 2)
+        self.VerifyFlags_NoMod_LDY()
+
+    def test_LDY_IM_ZERO(self):
+        self.cpu.Y_reg = 0x44
+        self.mem[0xFFFC] = self.cpu.INS_LDY_IM
+        self.mem[0xFFFD] = 0x00
+        CyclesUsed = self.cpu.exec(self.mem, 2)
+        self.assertEqual(self.cpu.Y_reg, 0x00)
+        self.assertEqual(CyclesUsed, 2)
+        self.VerifyFlags_NoMod_LDY()
+
+    def test_LDY_ZP(self):
+        self.mem[0xFFFC] = self.cpu.INS_LDY_ZP
+        self.mem[0xFFFD] = 0x42
+        self.mem[0x0042] = 0x37
+        CyclesUsed = self.cpu.exec(self.mem, 3)
+        self.assertEqual(self.cpu.Y_reg, 0x37)
+        self.assertEqual(CyclesUsed, 3)
+        self.VerifyFlags_NoMod_LDY()
+
+    def test_LDY_ZPX(self):
+        self.cpu.X_reg = 5
+        self.mem[0xFFFC] = self.cpu.INS_LDY_ZPX
+        self.mem[0xFFFD] = 0x42
+        self.mem[0x0047] = 0x37
+        CyclesUsed = self.cpu.exec(self.mem, 4)
+        self.assertEqual(self.cpu.Y_reg, 0x37)
+        self.assertEqual(CyclesUsed, 4)
+        self.VerifyFlags_NoMod_LDY()
+
+    def test_LDY_ABS(self):
+        self.mem[0xFFFC] = self.cpu.INS_LDY_ABS
+        self.mem[0xFFFD] = 0x80
+        self.mem[0xFFFE] = 0x44
+        self.mem[0x4480] = 0x84
+        CyclesUsed = self.cpu.exec(self.mem, 4)
+        self.assertEqual(self.cpu.Y_reg, 0x84)
+        self.assertEqual(CyclesUsed, 4)
+        self.VerifyFlags_NoMod_LDY()
+
+    def test_LDY_ABSX(self):
+        self.cpu.X_reg = 0x45
+        self.mem[0xFFFC] = self.cpu.INS_LDY_ABSX
+        self.mem[0xFFFD] = 0x45
+        self.mem[0xFFFE] = 0x01
+        self.mem[0x0145 + self.cpu.X_reg] = 0x84
+        CyclesUsed = self.cpu.exec(self.mem, 4)
+        self.assertEqual(self.cpu.Y_reg, 0x84)
+        self.assertIn(CyclesUsed, [4, 5])
+        self.VerifyFlags_NoMod_LDY()
+
+    def test_LDY_ABSX_Wrap(self):
+        self.cpu.X_reg = 0x0F
+        self.mem[0xFFFC] = self.cpu.INS_LDY_ABSX
+        self.mem[0xFFFD] = 0xF5
+        self.mem[0xFFFE] = 0xFF
+        self.mem[0xFFF5 + self.cpu.X_reg] = 0x84
+        CyclesUsed = self.cpu.exec(self.mem, 5)
+        self.assertEqual(self.cpu.Y_reg, 0x84)
+        self.assertIn(CyclesUsed, [4, 5])
+        self.VerifyFlags_NoMod_LDY()
+
+    def test_NOP(self):
+        self.mem[0xFFFC] = self.cpu.INS_NOP
+        CyclesUsed = self.cpu.exec(self.mem, 2)
+        self.assertEqual(CyclesUsed, 2)
 
     def test_NOP_IF_CYCLES_EQ_0(self):
         CyclesUsed = self.cpu.exec(self.mem, 0)
@@ -120,6 +351,18 @@ class TestComputer(unittest.TestCase):
 
     def test_LDA_IM_LT_NC(self):
         self.mem[0xFFFC] = self.cpu.INS_LDA_IM
+        self.mem[0xFFFD] = 0x84
+        CyclesUsed = self.cpu.exec(self.mem, 1)
+        self.assertEqual(CyclesUsed, 2)
+
+    def test_LDX_IM_LT_NC(self):
+        self.mem[0xFFFC] = self.cpu.INS_LDX_IM
+        self.mem[0xFFFD] = 0x84
+        CyclesUsed = self.cpu.exec(self.mem, 1)
+        self.assertEqual(CyclesUsed, 2)
+
+    def test_LDY_IM_LT_NC(self):
+        self.mem[0xFFFC] = self.cpu.INS_LDY_IM
         self.mem[0xFFFD] = 0x84
         CyclesUsed = self.cpu.exec(self.mem, 1)
         self.assertEqual(CyclesUsed, 2)
